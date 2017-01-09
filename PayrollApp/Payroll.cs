2c027c9294c7f2c6e0cb1f2d228c80494aba96b4
mudrakpatel <mudrakpatel96@gmail.com>
2017-01-09 13:55:21 -0500

    Initail commit

--- PayrollApp/Payroll.cs
+++ PayrollApp/Payroll.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PayrollApp
 {
-    public partial class Form1 : Form
+    public partial class Payroll : Form
     {
-        public Form1()
+        public Payroll()
         {
             InitializeComponent();
         }
--- PayrollApp/Program.cs
+++ PayrollApp/Program.cs
@@ -16,7 +16,7 @@ namespace PayrollApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Payroll());
         }
     }
 }
